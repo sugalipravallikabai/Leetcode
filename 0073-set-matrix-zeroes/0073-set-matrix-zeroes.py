5cdda45827f240435602bcf1806f40d80512eb2d
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-07-23 18:27:44 +0530

    Time: 103 ms (55.19%), Space: 17.6 MB (5.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,25 +5,39 @@ class Solution:
         """
         
         r,c = len(m),len(m[0])
-        
-        t1 = [0] * r
-        t2 = [0] * c
-        
+        c1 = 0
         for i in range(r):
             for j in range(c):
                 
                 if m[i][j] == 0:
                     
-                    t1[i] = 1
-                    t2[j] = 1
+                    m[i][0] = 0
                     
-        for i in range(r):
+                    if j == 0:
+                        
+                        c1 = 1
+                    
+                    else:
+                        m[0][j] = 0
+        
+        for i in range(1,r):
+            for j in range(1,c):
+                if m[i][j] != 0:
+                    if m[i][0] == 0 or m[0][j]  == 0:
+                        m[i][j] = 0
+        
+        if m[0][0] == 0:
             for j in range(c):
                 
-                if t1[i] == 1 or t2[j] == 1:
-                    
-                    m[i][j] = 0
+                m[0][j] = 0
+        if c1 == 1:
+            for i in range(r):
+                m[i][0] = 0
+        
         return m
+            
+        
+                
                     
                     
         
