e245d2762f66e2737fe07401a6f9e27894bab6b7
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-07-30 23:15:33 +0530

    Time: 179 ms (79.48%), Space: 17.7 MB (13.88%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,23 +1,24 @@
 class Solution:
     def findCircleNum(self, a : List[List[int]]) -> int:
-
-        if  not a:
-            return 0
-        n = len(a)
-        temp = [0] * n
         
-        def dfs(i):
+        if not a:
+            return 0
+        
+        n = len(a)
+        temp = [0]*n
+        cnt = 0
+        
+        def dfs(x):
+            # temp[x] = 1
             for j in range(n):
-                if a[i][j] == 1 and temp[j] != 1:
+                if a[x][j] == 1 and temp[j] != 1:
                     temp[j] = 1
                     dfs(j)
         
-        cnt = 0
         for i in range(n):
             if temp[i] == 0:
-                temp[i] = 1
-                cnt += 1
                 dfs(i)
-                
+                temp[i] = 1
+                cnt+=1
         return cnt
-                
+            
