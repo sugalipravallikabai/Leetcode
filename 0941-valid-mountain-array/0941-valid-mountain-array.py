1d89cf178be711829b712492f9f10f663ca99373
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-08-05 22:08:25 +0530

    Time: 157 ms (61.00%), Space: 17.9 MB (62.35%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,18 +1,16 @@
 class Solution:
     def validMountainArray(self, a : List[int]) -> bool:
-        flag = 0
-        if len(a) <= 2:
+        
+        n = len(a)
+        i = 0
+        if n < 3:
             return False
-        if a[0] > a[1]:
+        
+        while i < n-1 and a[i] < a[i+1]:
+            i += 1
+        if i == 0 or i == n-1:
             return False
-        for i in range(len(a)-1):
-            if a[i] == a[i+1]:
-                return False
-            if a[i] > a[i+1]:
-                flag = 1
-            if flag == 1 and a[i] < a[i+1]:
-                return False
-        if flag == 1:
-            return True
-        return False
+        while i < n-1 and a[i] > a[i+1]:
+            i += 1
+        return i == n-1
         
