1f41825eaba5f7a5d6df68de144a0a54e232aca9
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-08-05 17:17:18 +0530

    Time: 886 ms (10.18%), Space: 59.5 MB (5.97%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.py
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -1,28 +1,29 @@
-from math import sqrt
+
 class Solution:
     def diagonalPrime(self, n : List[List[int]]) -> int:
         
-        d1 = []
         maxi = 0
+        d = []
         for i in range(len(n)):
-            for j in range(len(n[0])):
-                if i - j == 0:
-                    d1.append(n[i][j])
-                elif i+j == len(n)-1:
-                    d1.append(n[i][j])
-        for val in d1:
-            if val == 1:
-                maxi = 0
-            elif val <= 3:
-                maxi = max(maxi,val)
-                continue
-            flag = 0
-            for i in range(2,int(math.sqrt(val))+1):
-                if val % i == 0:
-                    flag = 1
-                    break
-            if flag == 0 and val != 1:
-                maxi = max(maxi,val)
-        return maxi
+            if i < len(n):
+                d.append(n[i][i])
+                maxi = max(maxi,n[i][i])
+            if i < len(n):
+                d.append(n[i][len(n)-1-i])
+                maxi = max(maxi,n[i][len(n)-1-i])
+        
+        temp = [True] * (maxi+1)
+        temp[0]=temp[1]= False
+        for i in range(isqrt(maxi)+1):
+            if temp[i]:
+                for j in range(i*i,maxi+1,i):
+                    temp[j] = False
+        ans = 0
+        for val in d:
+            if temp[val]:
+                ans = max(ans,val)
+        return ans
+                
+        
             
         
