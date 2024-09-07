3f4859f0a1533bf929180a2f6b0ac0748244cb47
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-09-07 22:53:08 +0530

    Time: 154 ms (72.72%), Space: 28.1 MB (27.96%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,11 +1,11 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotate(self, n : List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k%len(nums)
-        r = len(nums) - k
-        res = nums[:r]
-        nums[:r] = []
-        return nums.extend(res)
+        k = k%len(n)
+        n.reverse()
+        n[:k] = reversed(n[:k])
+        n[k:] = reversed(n[k:])
+        return n
         
