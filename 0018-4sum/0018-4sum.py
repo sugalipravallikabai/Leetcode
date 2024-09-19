0de751c53551104960fd0333a2d0f52a9cfe8b03
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-09-19 12:59:58 +0530

    Time: 591 ms (40.54%), Space: 11.7 MB (47.58%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,32 +1,33 @@
-class Solution:
-    def fourSum(self, n: List[int], tar: int) -> List[List[int]]:
+class Solution(object):
+    def fourSum(self, n , t):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
         n.sort()
-        k = []
-        
-        
+        a = []
+        k = 0
+        l = len(n)-1
         for i in range(len(n)):
-            if i > 0 and n[i] == n[i-1] :
+            if i > 0 and n[i] == n[i-1]:
                 continue
             for j in range(i+1,len(n)):
-                if n[j]  == n[j-1] and j > i+1:
+                if j > i+1 and n[j] == n[j-1]:
                     continue
-                l=j+1
-                r=len(n)-1
-                
-                while l < r:
-                    tol = n[i] + n[j] + n[l] + n[r]
-                    if tol == tar :
-                        
-                        k.append([n[i],n[j],n[l],n[r]])
-                        
-                        while l < r and n[l] == n[l+1] :  
-                            l+=1
-                        while l < r and n[r] == n[r-1] :
-                            r-=1
-                        l+=1
-                        r-=1
-                    elif tol < tar:
-                        l += 1
+                k = j+1
+                l = len(n)-1
+                while k < l:
+                    s = n[i]+n[j]+n[k]+n[l]
+                    if s == t:
+                        a.append([n[i],n[j],n[k],n[l]])
+                        while k < l and n[k] == n[k+1]: k += 1
+                        while k < l and n[l] == n[l-1]: l -= 1
+                        k += 1
+                        l -= 1
+                    elif s < t:
+                        k += 1
                     else:
-                        r-=1
-        return k
+                        l -= 1
+        return a
+        
