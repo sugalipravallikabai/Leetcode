0249a0f6bd33728408da3b508ddcaa5bf557e671
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-09-24 00:08:10 +0530

    Time: 31 ms (88.92%), Space: 16.5 MB (79.80%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,34 @@
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
-        def fun(row):
-            ans = []
-            ans.append(1)
-            val = 1
-            for i in range(1,row):
-                val *= (row-i)
-                val //= i
-                ans.append(val)
-            return ans
+#         def fun(row):
+#             ans = []
+#             ans.append(1)
+#             val = 1
+#             for i in range(1,row):
+#                 val *= (row-i)
+#                 val //= i
+#                 ans.append(val)
+#             return ans
         
-        a = []
-        for i in range(1,n+1):
-            a.append(fun(i))
-        return a
+#         a = []
+#         for i in range(1,n+1):
+        #     a.append(fun(i))
+        # return a
+        
+
+
+#           using dp approch
+        t = []
+        
+        for i in range(n):
+            row = [1]*(i+1)
+            
+            for j in range(1,i):
+                row[j] = t[i-1][j-1]+t[i-1][j]
+            
+            t.append(row)
+        
+        return t
+    
+    
+    
