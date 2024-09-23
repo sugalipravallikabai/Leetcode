c3bc40efee5c5fde6ca7084d46d892d8d878baf5
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-09-23 23:46:22 +0530

    Time: 32 ms (82.85%), Space: 16.6 MB (32.20%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,11 +1,12 @@
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
         def fun(row):
-            ans=[1]
+            ans = []
+            ans.append(1)
             val = 1
             for i in range(1,row):
                 val *= (row-i)
-                val //= (i)
+                val //= i
                 ans.append(val)
             return ans
         
