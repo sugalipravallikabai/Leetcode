9d5f5130bfa72aec763d94bceaa742bed83d9696
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-10-22 22:29:45 +0530

    Time: 0 ms (100.00%), Space: 16.9 MB (37.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,20 +1,18 @@
 class Solution:
     def nextGreaterElement(self, n1: List[int], n2: List[int]) -> List[int]:
-        ans = [-1]*len(n1)
-        for i in range(len(n1)):
-            d = n1[i]
-            st = []
-            for j in range(len(n2)-1,-1,-1):
-                if d == n2[j]:
-                    while st and st[-1] <= d:
-                        st.pop()
-                    if not st:
-                        ans[i] = -1
-                    else:
-                        ans[i] = st[-1]
-                        break
-                else:
-                    st.append(n2[j])
-        return ans
+        d = {}
+        st = []
+        for i in reversed(n2):
+            
+            while st and st[-1] < i:
+                st.pop()
+            if st:
+                d[i] = st[-1]
+            else:
+                d[i] = -1
+            
+            st.append(i)
+            
+        return [d[num] for num in n1]
         
         
