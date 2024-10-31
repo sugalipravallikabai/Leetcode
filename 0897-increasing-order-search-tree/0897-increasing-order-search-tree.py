dd829ae06e8f54c865e417c776952b7e9d30ff1d
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-10-31 18:42:58 +0530

    Time: 0 ms (100.00%), Space: 16.8 MB (24.83%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.py
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.py
@@ -6,18 +6,26 @@
 #         self.right = right
 class Solution:
     def increasingBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        ptr = TreeNode()
-        res  = ptr
-        def fun(node):
-            nonlocal ptr
-            if not node:
-                return
-            fun(node.left)
-            ptr.right = TreeNode(node.val)
-            ptr = ptr.right
-            fun(node.right)
-        
-        fun(root)
-        return res.right
+        dummy  = TreeNode(-1)
+        temp = dummy
+        cur = root
+        while cur:
+            if not cur.left:
+                temp.right = cur
+                cur = cur.right
+                temp = temp.right
+            else:
+                pred = cur.left
+                while pred.right and pred.right != cur:
+                    pred = pred.right
+                if not pred.right:
+                    pred.right = cur
+                    cur = cur.left
+                else:
+                    temp.right = cur
+                    cur.left = None
+                    temp = temp.right
+                    cur = cur.right
+        return dummy.right
             
         
