402fdc0861ce452de985d63040cf435dd8781b4e
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-11-05 20:19:12 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (23.51%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        l,r = 0,0
+        for c in s:
+            if c == '(':
+                l += 1
+                r += 1
+            elif c ==')':
+                l -= 1
+                r -= 1
+            else:
+                l -= 1
+                r += 1
+            if r < 0:
+                return False
+            if l < 0:
+                l = 0
+        return l == 0
