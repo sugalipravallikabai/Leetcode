3a131b3d57b2650bbe59072ab382d83504b654a0
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-11-07 01:19:26 +0530

    Time: 4 ms (14.92%), Space: 16.7 MB (12.55%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,7 +1,6 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         st = []
-        cnt = 0
         for c in s:
             if c == '(':
                 st.append(c)
@@ -10,5 +9,4 @@ class Solution:
                     st.pop()
                 else:
                     st.append(c)
-                    cnt+=1
         return len(st)
