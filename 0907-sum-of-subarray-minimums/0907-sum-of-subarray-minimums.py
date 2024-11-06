812a5a6ae6472785601c5ef8cbc842c2edb9f216
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-11-06 20:26:29 +0530

    Time: 115 ms (24.03%), Space: 20.5 MB (90.08%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,52 +1,29 @@
 class Solution:
-    def sumSubarrayMins(self, a : List[int]) -> int:
-        
+    def sumSubarrayMins(self, a: List[int]) -> int:
+        nse = [len(a)]*len(a)
+        st = []
         mod = 10**9+7
-        def fun1(a):
-            n = len(a)
-            st = []
-            nse = [0]*n
-            for i in range(n-1,-1,-1):
-                while st and a[st[-1]] >= a[i]:
-                    st.pop()
-                if not st:
-                    nse[i] = n
-                else:
-                    nse[i] = st[-1]
-                st.append(i)
-            return nse
-        def fun2(a):
-            n = len(a)
-            st = []
-            pse = [0]*n
-            for i in range(n):
-                while st and a[st[-1]] > a[i]:
-                    st.pop()
-                if not st:
-                    pse[i] = -1
-                else:
-                    pse[i] = st[-1]
-                st.append(i)
-            return pse
-        
-        tol = 0
-        nse = fun1(a)
-        pse = fun2(a)
+        for i in range(len(a)-1,-1,-1):
+            while st and a[st[-1]] >= a[i]:
+                st.pop()
+            if st:
+                nse[i] = st[-1]
+            st.append(i)
+        pse = [-1]*len(a)
+        st = []
         for i in range(len(a)):
-            left = i-pse[i]
-            right = nse[i]-i
-            tol = (tol+((left*right*a[i]) % mod)) % mod
+            while st and a[st[-1]] > a[i]:
+                st.pop()
+            if st:
+                pse[i]=st[-1]
+            st.append(i)
+        # print(nse,pse)
+        tol = 0
+        for i in range(len(a)):
+            left = nse[i] - i
+            right = i -pse[i]
+            tol = (tol + (left*right*a[i])%mod)%mod
+        
         return tol
             
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
