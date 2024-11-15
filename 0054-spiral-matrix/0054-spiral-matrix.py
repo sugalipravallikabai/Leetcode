163c44106071c50069e428d326379a7e5537c8bf
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-11-15 22:49:08 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (93.66%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,31 +1,25 @@
 class Solution:
-    def spiralOrder(self, m : List[List[int]]) -> List[int]:
-        
-        r,c = len(m),len(m[0])
-        left = 0
-        right = c-1
-        top = 0
-        btm = r-1
-        ans = []
-        
-        while top <= btm and left <= right:
-            for i in range(left,right+1):
-                ans.append(m[top][i])
-            top += 1
-            if not (top <= btm and left <= right):
-                break
-            for i in range(top,btm+1):
-                ans.append(m[i][right])
-            right -= 1
-            if not (top <= btm and left <= right):
-                break
-            for i in range(right,left-1,-1):
-                ans.append(m[btm][i])
-            btm -= 1
-            if not (top <= btm and left <= right):
-                break
-            for i in range(btm,top-1,-1):
-                ans.append(m[i][left])
-            left += 1
+    def spiralOrder(self, a : List[List[int]]) -> List[int]:
+        ans  = []
+        left = top = 0
+        n = len(a)
+        m = len(a[0])
+        right = m-1
+        bottom = n-1
+        while top <= bottom and left <= right:
             
+            for i in range(left,right+1):
+                ans.append(a[top][i])
+            top += 1
+            for i in range(top,bottom+1):
+                ans.append(a[i][right])
+            right -= 1
+            if top <= bottom:
+                for i in range(right,left-1,-1):
+                    ans.append(a[bottom][i])
+                bottom -= 1
+            if left <= right:
+                for i in range(bottom,top-1,-1):
+                    ans.append(a[i][left])
+                left += 1
         return ans
