bb9823d21d634091c6155d50fad021dc059fe79b
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-11-28 18:52:19 +0530

    Time: 0 ms (100.00%), Space: 17.1 MB (11.29%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, n : List[int]) -> int:
+        if len(n) == 0:
+            return 0
+        if len(n) == 1:
+            return n[0]
+        pre1 = n[0]
+        pre2 = max(n[0],n[1])
+        
+        for i in range(2,len(n)):
+            cur = max(pre2,pre1+n[i])
+            pre1 = pre2
+            pre2 = cur
+        return pre2
