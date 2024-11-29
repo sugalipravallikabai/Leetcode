3b9d864a9a91fdeed6a7913d3a0b652d1906ef91
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-11-29 13:31:06 +0530

    Time: 0 ms (100.00%), Space: 19.1 MB (8.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,29 +1,15 @@
 class Solution:
-    def findCircleNum(self, a : List[List[int]]) -> int:
-        
-        if not a:
-            return 0
-        
-        n = len(a)
-        temp = [0]*n
-        cnt = 0
-        q = deque()
-        
-        def bfs(x):
-            q.append(x)
-            while q:
-                node = q.popleft()
-                temp[node] = 1
-                for j in range(n):
-                    if a[node][j] == 1 and temp[j] !=1:
-                        q.append(j)
-                        temp[j] = 1
-            
-        
+    def findCircleNum(self, icon : List[List[int]]) -> int:
+        n = len(icon)
+        visit = [0]*n
+        cnt  = 0
+        def fun(r):
+            visit[r] = 1
+            for j in range(n):
+                if visit[j] != 1 and icon[r][j] == 1:
+                    fun(j)
         for i in range(n):
-            if temp[i] == 0:
-                bfs(i)
-                # tem[i] = 1
-                cnt += 1
+            if visit[i] != 1 :
+                    cnt += 1
+                    fun(i)
         return cnt
-            
