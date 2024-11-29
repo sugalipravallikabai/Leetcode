f4ba0a2a29542e726c52b4ecb04a412f00d68876
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-11-29 15:47:41 +0530

    Time: 7 ms (62.31%), Space: 21.4 MB (21.78%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,48 +1,32 @@
 class Solution:
-    def solve(self, b : List[List[str]]) -> None:
+    def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not b or not b[0]:
-            return b
-        n,m = len(b) , len(b[0])
-        visit = [[0]*m for _ in range(n)]
-        
-        def dfs(r,c,v,b):
-            
-            v[r][c] = 1
-            
-            for dr,dc in ([-1,0],[1,0],[0,-1],[0,1]):
-                row = r+dr
-                col = c+dc
-                
-                if 0 <= row < n and  0 <= col < m and v[row][col] != 1 and b[row][col] == 'O':
-                    dfs(row,col,v,b)
-        
-        for j in range(m):
-            
-            if visit[0][j] != 1 and b[0][j] == 'O':
-                
-                dfs(0,j,visit,b)
-            
-            if visit[n-1][j] != 1 and b[n-1][j] == 'O':
-                
-                dfs(n-1,j,visit,b)
-        
+        n,m = len(board),len(board[0])
+        def dfs(i,j):
+            if i < 0 or i >= n or j < 0 or j >= m or board[i][j] != 'O':
+                return 
+            board[i][j] = 'T'
+            for r,c in ([1,0],[-1,0],[0,1],[0,-1]):
+                dfs(r+i,c+j)
         for i in range(n):
-            
-            if visit[i][0] != 1 and b[i][0] == 'O':
-                
-                dfs(i,0,visit,b)
-                
-            if visit[i][m-1] != 1 and b[i][m-1] == 'O':
-                
-                dfs(i,m-1,visit,b)
+            if board[i][0] == 'O':
+                dfs(i,0)
+            if board[i][m-1] == 'O':
+                dfs(i,m-1)
+        for j in range(m):
+            if board[0][j] == 'O':
+                dfs(0,j)
+            if board[n-1][j] == 'O':
+                dfs(n-1,j)
         
         for i in range(n):
             for j in range(m):
-                if b[i][j] == 'O' and visit[i][j] == 0:
-                    
-                    b[i][j] = 'X'
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
+        return board
         
-        return b
+        
