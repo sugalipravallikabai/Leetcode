85545dacc8436001f2a3beb563a9c5386b15a19a
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-06 18:59:08 +0530

    Time: 55 ms (69.76%), Space: 18 MB (5.50%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -9,7 +9,7 @@ class Solution:
         while r < n:
             if nums[r] == 0:
                 zeros += 1
-            while zeros > k:
+            if zeros > k :
                 if nums[l] == 0:
                     zeros -= 1
                 l += 1
