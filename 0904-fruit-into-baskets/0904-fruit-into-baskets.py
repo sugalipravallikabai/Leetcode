9d1f6a04fbf409c47536931f667d1d0b12bc2f7a
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-07 23:02:02 +0530

    Time: 194 ms (39.50%), Space: 23.3 MB (29.88%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        if not fruits:
+            return 0
+        n = len(fruits)
+        maxi = 0
+        d = {}
+        r,l = 0,0
+        while r < n:
+            if fruits[r] in d:
+                d[fruits[r]] += 1
+            else:
+                d[fruits[r]] = 1
+            if len(d) > 2:
+                while len(d) > 2:
+                    d[fruits[l]] -= 1
+                    if d[fruits[l]] == 0:
+                        del d[fruits[l]]
+                    l += 1
+                    
+            if len(d) <= 2:
+                maxi = max(maxi,r-l+1)
+            r += 1
+        return maxi
