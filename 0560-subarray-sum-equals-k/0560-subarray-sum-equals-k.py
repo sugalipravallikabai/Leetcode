8e19934da60a3b187a72a266a061fc22c84617bf
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-08 02:14:46 +0530

    Time: 31 ms (65.83%), Space: 19.9 MB (26.49%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,66 +1,18 @@
 class Solution:
-    def subarraySum(self, a : List[int], k: int) -> int:
-        
-        #  applicable only for positive numbers....
-        
-#         cnt = 0
-#         tol = a[0]
-#         n = len(a)
-#         i,j = 0,0
-        
-#         if n == 1:
-#             if tol != k:
-#                 return 0
-        
-#         while j < n:
-#             while tol > k and i <= j:
-                
-#                 tol -= a[i]
-#                 i+=1
-#             if tol == k:
-#                 cnt += 1
-#             j+=1
-#             if j < n:
-#                 tol += a[j]
-        # return cnt
-        
-        cnt = 0
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return 0
         d = {}
-        ps = 0
-        n = len(a)
-        
+        n,tol,cnt=len(nums),0,0
         for i in range(n):
-            ps += a[i]
-            
-            if ps == k:
+            tol += nums[i]
+            if tol == k:
                 cnt += 1
-            if ps-k in d:
-                cnt +=  d[ps-k]
-            if ps not in d:
-                d[ps] = 1
+            if tol - k in d:
+                cnt += d[tol-k]
+            if tol in d:
+                d[tol] += 1
             else:
-                d[ps] += 1
+                d[tol] = 1
         return cnt
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      
+                
