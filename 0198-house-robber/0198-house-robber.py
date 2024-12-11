c5dacd39d8e9d0d1ef903241f438197647b3f124
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-11 11:21:48 +0530

    Time: 0 ms (100.00%), Space: 17.1 MB (17.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -7,7 +7,7 @@ class Solution:
         pre1 = n[0]
         pre2 = max(n[0],n[1])
         for i in range(2,len(n)):
-            cur = max(pre1+n[i],pre2)
+            cur = max(n[i]+pre1,pre2)
             pre1 = pre2
             pre2 = cur
         return pre2
