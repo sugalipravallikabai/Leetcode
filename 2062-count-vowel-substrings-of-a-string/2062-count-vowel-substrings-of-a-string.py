4de1438c2c733dd8e178bed47fbf33ca229ccc26
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-13 13:26:45 +0530

    Time: 13 ms (61.99%), Space: 17.3 MB (11.29%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -3,11 +3,14 @@ class Solution:
         cnt = 0
         for i in range(len(word)):
             s = set()
-            for j in range(i,len(word)):
-                if word[j] in 'aeiou':
-                    s.add(word[j])
-                    if len(s) >= 5:
-                        cnt += 1
-                else:
-                    break
+            if word[i] in 'aeiou':
+                for j in range(i,len(word)):
+                    if word[j] in 'aeiou':
+                        s.add(word[j])
+                        if len(s) >= 5:
+                            cnt += 1
+                    else:
+                        break
+            else:
+                i = i+1
         return cnt
