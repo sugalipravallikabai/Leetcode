c101809793c5cc27d7468ec19a4218c4777309a9
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-17 20:11:39 +0530

    Time: 7580 ms (11.36%), Space: 133.6 MB (10.69%) - LeetHub

---
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
+        
+        n = len(nums)
+        k = target
+        dp = [[-1 for _ in range(k + 1)] for _ in range(n)]
+        def fun(i, k):
+            if k < 0:
+                return float('-inf')
+            if i == 0:
+                if k == 0:
+                    return 0
+                if k == nums[i]:
+                    return 1
+                return float('-inf')
+            
+            if dp[i][k] != -1:
+                return dp[i][k]
+
+            notpick = 0+fun(i - 1, k)
+            pick = float('-inf')
+            if nums[i] <= k:
+                pick = 1+fun(i - 1, k - nums[i])
+            
+            dp[i][k] = max(pick,notpick)
+            return dp[i][k]
+        ans = fun(n - 1, k)
+        return ans if ans > 0 else -1
