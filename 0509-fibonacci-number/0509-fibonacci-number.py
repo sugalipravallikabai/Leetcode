20d6f3a2f6dba370fcea4c83f70f19808bd1a0c1
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-20 17:21:51 +0530

    Time: 34 ms (72.43%), Space: 17.7 MB (11.10%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        pre,pre1 = 0,1
-        for i in range(2,n+1):
-            cur = pre+pre1
-            pre = pre1
-            pre1 = cur
-        return pre1
+        if n <= 2:
+            if n == 0:
+                return 0
+            return 1
+        pre = 0
+        pr1 = 1
+        for i in range(n-1):
+            cur = pre+pr1
+            pre = pr1
+            pr1 = cur
+        return pr1
