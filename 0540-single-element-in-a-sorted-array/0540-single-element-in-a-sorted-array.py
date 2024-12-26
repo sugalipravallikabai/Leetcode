93cafcf5491f970e457e4fb2977b6c2449c99015
Sugali Pravallika Bai <110448588+sugalipravallikabai@users.noreply.github.com>
2024-12-26 23:33:31 +0530

    Time: 0 ms (100.00%), Space: 24.9 MB (8.80%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,21 +1,18 @@
 class Solution:
-    def singleNonDuplicate(self, n : List[int]) -> int:
-        x = 0
-        l = 1
-        r = len(n)-2
-        
-        if len(n) == 1:
-            return n[0]
-        if n[0] != n[1]:
-            return n[0]
-        if n[len(n)-1] != n[len(n)-2]:
-            return n[len(n)-1]
-        
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        n = len(nums)
+        if nums[0] != nums[1] :
+            return nums[0]
+        if nums[n-1] != nums[n-2]:
+            return nums[n-1]
+        l,r = 0,n-2
         while l <= r:
             m = (l+r)//2
-            if n[m] != n[m+1] and n[m] != n[m-1]:
-                return n[m]
-            elif (m % 2 == 0 and n[m+1] == n[m]) or (m%2 == 1 and n[m-1] == n[m]):
+            if nums[m] != nums[m-1] and nums[m] != nums[m+1]:
+                return nums[m]
+            elif (m % 2 == 1 and nums[m] == nums[m-1]) or (m % 2 == 0 and nums[m] == nums[m+1])  :
                 l = m+1
             else:
                 r = m-1
